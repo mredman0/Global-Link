eaf7b49d59dcd622c90265ea01fda4c29463ba2b
Mark <mredman@udel.edu>
2024-10-29 02:21:07 -0400

    Fix build

--- Assets/PuzzleBuilder/PuzzlePackGenerator.cs
+++ Assets/PuzzleBuilder/PuzzlePackGenerator.cs
@@ -1,3 +1,4 @@
+#if ( UNITY_EDITOR )
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -285,3 +286,4 @@ public class PuzzlePackGenerator : MonoBehaviour
         public Vector2Int[] Seeds;
     }
 }
+#endif
