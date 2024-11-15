5166c0e5c6a906a51238ae6ffa1d1fae3b0f9ca3
Mark <mredman@udel.edu>
2024-11-14 19:30:52 -0500

    Puzzle pack buttons show checkmark if complete

--- Assets/Scripts/UI/PuzzlePackButton.cs
+++ Assets/Scripts/UI/PuzzlePackButton.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzlePackButton : MonoBehaviour
 {
     [Header("Required References")]
     public TMP_Text PackNameText;
     public TMP_Text CompletedPuzzlesText;
+    public Image PackCompleteCheckmark;
 
 
     [Header("Settings")]
@@ -20,5 +22,7 @@ public class PuzzlePackButton : MonoBehaviour
 
         var (completed, total) = PuzzleCompletionManager.Instance.GetPackStats(Pack.Id);
         CompletedPuzzlesText.text = $"{completed} / {total}";
+
+        PackCompleteCheckmark.gameObject.SetActive(completed >= total);
     }
 }
