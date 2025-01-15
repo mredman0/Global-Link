3b379730eaa746e757c1c47438e146f7064b4c4e
Mark <mredman@udel.edu>
2025-01-15 14:35:09 -0500

    Possible fix for iOS achievements

--- Assets/Scripts/Managers/AchievementManager.cs
+++ Assets/Scripts/Managers/AchievementManager.cs
@@ -10,6 +10,8 @@ public class AchievementManager : MonoBehaviour
     public static AchievementManager Instance;
 
     private Dictionary<Achievement, string> AchievementIds = new Dictionary<Achievement, string>();
+    private Dictionary<string, Achievement> AchievementsById = new Dictionary<string, Achievement>();
+    private Dictionary<Achievement, double> AchievementProgress = new Dictionary<Achievement, double>();
 
     // Start is called before the first frame update
     void Start()
@@ -37,39 +39,51 @@ public class AchievementManager : MonoBehaviour
     private void OnPlayerAuthenticated()
     {
         AchievementIds.Clear();
-#if UNITY_ANDROID
-        AchievementIds.Add(Achievement.PacksBeginner, "CgkIqpOH0vcZEAIQBg");
-        AchievementIds.Add(Achievement.PacksIntermediate, "CgkIqpOH0vcZEAIQBw");
-        AchievementIds.Add(Achievement.PacksExpert, "CgkIqpOH0vcZEAIQCA");
-        AchievementIds.Add(Achievement.PacksGrandmaster, "CgkIqpOH0vcZEAIQCQ");
+#if !UNITY_EDITOR
+        DefineAchievement(Achievement.PacksBeginner, "CgkIqpOH0vcZEAIQBg");
+        DefineAchievement(Achievement.PacksIntermediate, "CgkIqpOH0vcZEAIQBw");
+        DefineAchievement(Achievement.PacksExpert, "CgkIqpOH0vcZEAIQCA");
+        DefineAchievement(Achievement.PacksGrandmaster, "CgkIqpOH0vcZEAIQCQ");
 
-        AchievementIds.Add(Achievement.DailyNovice, "CgkIqpOH0vcZEAIQAw");
-        AchievementIds.Add(Achievement.DailyAdept, "CgkIqpOH0vcZEAIQBA");
-        AchievementIds.Add(Achievement.DailyGuru, "CgkIqpOH0vcZEAIQBQ");
+        DefineAchievement(Achievement.DailyNovice, "CgkIqpOH0vcZEAIQAw");
+        DefineAchievement(Achievement.DailyAdept, "CgkIqpOH0vcZEAIQBA");
+        DefineAchievement(Achievement.DailyGuru, "CgkIqpOH0vcZEAIQBQ");
 
-        AchievementIds.Add(Achievement.ManyTunnels, "CgkIqpOH0vcZEAIQCg");
-        AchievementIds.Add(Achievement.MultiTunnelLine, "CgkIqpOH0vcZEAIQCw");
-        AchievementIds.Add(Achievement.TunnelAvoider, "CgkIqpOH0vcZEAIQDA");
+        DefineAchievement(Achievement.ManyTunnels, "CgkIqpOH0vcZEAIQCg");
+        DefineAchievement(Achievement.MultiTunnelLine, "CgkIqpOH0vcZEAIQCw");
+        DefineAchievement(Achievement.TunnelAvoider, "CgkIqpOH0vcZEAIQDA");
 #endif
+#if !UNITY_EDITOR && UNITY_IOS
+        Social.LoadAchievements((achievements) =>
+        {
+            foreach(var achievement in achievements)
+            {
+                AchievementProgress.Add(AchievementsById[achievement.id], achievement.percentCompleted);
+            }
+        });
+#endif
+    }
+
+    private void DefineAchievement(Achievement ach, string id)
+    {
+        AchievementIds.Add(ach, id);
+        AchievementsById.Add(id, ach);
     }
 
     public void SetCompleted(Achievement ach) => SetProgress(ach, 100.0);
 
     public void SetProgress(Achievement ach, double percentDone)
     {
-#if UNITY_EDITOR
-        return;
-#endif
         if(!AchievementIds.ContainsKey(ach))
         {
             return;
         }
         var id = AchievementIds[ach];
 
-#if UNITY_ANDROID
-        PlayGamesPlatform.Instance.ReportProgress(id, percentDone, (success) =>
+#if UNITY_ANDROID || UNITY_IOS
+        Social.ReportProgress(id, percentDone, (success) =>
         {
-            if(success)
+            if (success)
             {
                 Debug.Log($"Achievement {id}: progress of {percentDone} reported");
             }
@@ -78,14 +92,19 @@ public class AchievementManager : MonoBehaviour
                 Debug.LogError($"Achievement {id}: progress of {percentDone} failed to be reported");
             }
         });
+        if(!AchievementProgress.ContainsKey(ach))
+        {
+            AchievementProgress.Add(ach, percentDone);
+        }
+        else
+        {
+            AchievementProgress[ach] = percentDone;
+        }
 #endif
     }
 
     public void Increment(Achievement ach, int steps = 1)
     {
-#if UNITY_EDITOR
-        return;
-#endif
         if (!AchievementIds.ContainsKey(ach))
         {
             return;
@@ -104,8 +123,42 @@ public class AchievementManager : MonoBehaviour
                 Debug.LogError($"Achievement {id}: failed to be incremented by {steps}");
             }
         });
+#elif UNITY_IOS
+        if (!AchievementIncrementSteps.ContainsKey(ach))
+        {
+            Debug.LogError($"Cannot increment achievement {ach} progress, it is not defined as incrementable");
+            return;
+        }
+        var incrementProgress = ((double)steps / AchievementIncrementSteps[ach]) * 100;
+        var currentProgress = AchievementProgress.ContainsKey(ach) ? AchievementProgress[ach] : 0;
+        Social.ReportProgress(AchievementIds[ach], currentProgress + incrementProgress, (success) =>
+        {
+            if (success)
+            {
+                if (!AchievementProgress.ContainsKey(ach))
+                {
+                    AchievementProgress.Add(ach, currentProgress + incrementProgress);
+                }
+                else
+                {
+                    AchievementProgress[ach] = currentProgress + incrementProgress;
+                }
+                Debug.Log($"Achievement {id}: incremented by {steps}");
+            }
+            else
+            {
+                Debug.LogError($"Achievement {id}: failed to be incremented by {steps}");
+            }
+        });
 #endif
     }
+
+    private Dictionary<Achievement, int> AchievementIncrementSteps = new Dictionary<Achievement, int>()
+    {
+        { Achievement.DailyNovice, 10 },
+        { Achievement.DailyAdept, 100 },
+        { Achievement.DailyGuru, 1000 },
+    };
 
     public enum Achievement
     {
